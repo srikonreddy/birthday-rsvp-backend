387a46cd44e3e008866b6e48358f13dbcf55d41f
Sri Konreddy <srikonreddy@Sris-MacBook-Air.local>
2024-12-30 15:38:01 +0000

    undo

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const fs = require('fs');
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
